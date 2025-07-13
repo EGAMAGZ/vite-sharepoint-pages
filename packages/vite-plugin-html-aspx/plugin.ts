442800c9c6d7f27211e4fa05266196928d3b09d5
EGAMAGZ <46827955+EGAMAGZ@users.noreply.github.com>
2025-07-13 01:48:19 -0600

    turns html into aspx

--- packages/vite-plugin-html-aspx/plugin.ts
+++ packages/vite-plugin-html-aspx/plugin.ts
@@ -1,12 +1,27 @@
-import { type Plugin } from "vite";
+import {type Plugin } from "vite";
 import path from "node:path";
 export default function htmlToAspx(): Plugin {
   return {
     name: "vite-plugin-html-aspx",
     apply: "build",
     enforce: "post",
-    generateBundle: (_a, bundle) => {
-	    console.dir(bundle);
+    generateBundle: (_, bundle) => {
+      const updates: Record<string, typeof bundle[string]> = {};
+      for (const [fileName, asset] of Object.entries(bundle)) {
+        if (!fileName.endsWith(".html") || asset.type !== "asset") continue;
+        const baseName = path.basename(fileName, ".html");
+        const newFileName = `${baseName}.aspx`;
+
+        updates[newFileName] = {
+          ...asset,
+          fileName: newFileName,
+        };
+
+        delete bundle[fileName];
+      }
+      Object.entries(updates).forEach(([newName, updatedAsset], _) => {
+        bundle[newName] = updatedAsset;
+      });
     },
   };
 }
