3b8cdc311c237805136acb7561ddeed056bee514
EGAMAGZ <46827955+EGAMAGZ@users.noreply.github.com>
2025-07-14 16:06:05 -0600

    fixed import

--- example/vite-ts/vite.config.ts
+++ example/vite-ts/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import { htmlToAspx } from "@egamagz/vite-sharepoint-pages";
+import { htmlToAspx } from "@egamagz/vite-plugin-html-aspx";
 import { join, resolve } from "node:path";
 import { readdirSync } from "node:fs";
 
