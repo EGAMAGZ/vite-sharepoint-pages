49d94dbde77bdd61dec918bd9bdcbb3f43680e97
EGAMAGZ <46827955+EGAMAGZ@users.noreply.github.com>
2025-07-13 22:55:37 -0600

    moved to its one example directory

--- packages/vite-plugin-html-aspx/plugin.ts
+++ packages/vite-plugin-html-aspx/plugin.ts
@@ -1,22 +1,28 @@
-import {type Plugin } from "vite";
+import { type Plugin } from "vite";
 import path from "node:path";
-export default function htmlToAspx(): Plugin {
+import { toPascalCase } from "@std/text";
+
+export interface HtmlToAspXConfiguration {
+  fileNameTransform?: (fileName: string) => string;
+}
+
+export function htmlToAspx(config: HtmlToAspXConfiguration = {}): Plugin {
   return {
     name: "vite-plugin-html-aspx",
     apply: "build",
     enforce: "post",
     generateBundle: (_, bundle) => {
       const updates: Record<string, typeof bundle[string]> = {};
+
       for (const [fileName, asset] of Object.entries(bundle)) {
         if (!fileName.endsWith(".html") || asset.type !== "asset") continue;
         const baseName = path.basename(fileName, ".html");
-        const newFileName = `${baseName}.aspx`;
+        const newFileName = `${toPascalCase(baseName)}.aspx`;
 
         updates[newFileName] = {
           ...asset,
           fileName: newFileName,
         };
-
         delete bundle[fileName];
       }
       Object.entries(updates).forEach(([newName, updatedAsset], _) => {
